d390bcbfc658490f2a6f838d8cc78edbd5dc61c8
AMK1215 <aungmyokhaing1215@gmail.com>
2025-07-21 10:32:29 +0630

    onexbet logo

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -108,7 +108,7 @@
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
              <a href="{{ route('home') }}" class="brand-link">
-            <img src="{{ asset('assets/img/logo/logo/1.png') }}" alt="AdminLTE Logo"
+            <img src="{{ asset('assets/img/logo/1.png') }}" alt="AdminLTE Logo"
                 class="brand-image img-circle elevation-3" style="opacity: .8">
             <span class="brand-text font-weight-light">ONEXBET</span>
             </a> 
